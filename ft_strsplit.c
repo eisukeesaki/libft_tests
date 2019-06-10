8f7b0a64c310d3ec1e4992214ecf7b7a66787c67
Eisuke Esaki <eesaki@student.42.us.org>
2019-06-09 19:41:29 -0700

    ft_strsplit.c

---
+++ ft_strsplit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int		main(void)
+{
+	char	str[] = ",apple,,orange,banana,";
+	char	dlm = ',';
+	char	**dstr = NULL;
+	size_t	i = 0;
+
+	dstr = ft_strsplit(str, dlm);
+
+	while (dstr[i])
+	{
+		printf("%s\n", dstr[i]);
+		i++;
+	}
+
+	return (0);
+}
